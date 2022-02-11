b59b96b20689556b9dcad2f44d59c4d4270ef459
Bhuvanachandra26 <92016823+Bhuvanachandra26@users.noreply.github.com>
2022-02-11 13:10:41 +0530

    Delete leap_year.py

--- leap_year.py
+++
@@ -1,8 +0,0 @@
-input_year = int(input("Enter the Year to be checked: "))
-if(input_year < 10000) and (input_year > 999):
-    if (input_year % 400 == 0) or ((input_year % 4 == 0) and (input_year % 100 != 0)):
-      print("%d is a Leap Year" %input_year)
-    else:
-        print("%d is Not the Leap Year" %input_year)
-else:
-    print("Please enter valid year")
